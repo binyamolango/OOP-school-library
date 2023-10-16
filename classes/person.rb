9dc494bbb1ae286477252085a511f5b99e932cec
binyamolango <olangobinyam@gmail.com>
2023-10-16 15:35:03 +0300

    Add constructor and instance variables to the person class

--- classes/person.rb
+++ classes/person.rb
@@ -1,3 +1,21 @@
 class Person
+  attr_reader :id, :name, :age
+  attr_writer :name, :age
+
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+  end
+
+  def of_age?
+    if @age >= 18
+      true
+    else false
+    end
+  end
+end
+
+def can_use_services?
 
 end
