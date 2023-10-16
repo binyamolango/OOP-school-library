79f15a67546e6824cc33c78140ee4a8433de0097
binyamolango <olangobinyam@gmail.com>
2023-10-16 16:09:35 +0300

    Update can_use_service method

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,17 @@
 require "./person.rb"
 
 class Teacher < Person
+  attr_accessor :specialization
+
   def initialize(specialization)
-    super(27, "Abiyu")
+    super(37, "Abiyu")
     @specialization = specialization
   end
 
-  
+  def can_use_services?
+    true
+  end
 end
+
+teacher1 = Teacher.new("Computer Engineering")
+puts teacher1.specialization
