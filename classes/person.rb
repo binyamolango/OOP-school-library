c797bfb4017de73107a5c914ca05303c48516769
binyamolango <olangobinyam@gmail.com>
2023-10-16 15:48:30 +0300

    Add of_age and can_use_service method

--- classes/person.rb
+++ classes/person.rb
@@ -1,21 +1,31 @@
 class Person
-  attr_reader :id, :name, :age
+  attr_reader :id, :name, :age, :parent_permission
   attr_writer :name, :age
 
-  def initialize(name = "Unknown", age, parent_permission = true)
+  def initialize(age, name = "Unknown", parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   def of_age?
     if @age >= 18
-      true
-    else false
+      return true
+    else 
+      return false
     end
   end
 end
 
-def can_use_services?
+person = Person.new(14)
 
+def can_use_services?(person)
+  if person.of_age?() == true || person.parent_permission == true
+    return true
+  else
+    return false
+  end
 end
+
+puts can_use_services?(person)
