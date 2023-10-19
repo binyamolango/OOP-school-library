c0ff25d0d937cf8cdea288d8b879ebc145a987b1
binyamolango <olangobinyam@gmail.com>
2023-10-19 21:13:43 +0300

    Create a main.rb

---
+++ main.rb
@@ -0,0 +1,21 @@
+require "./app"
+
+def main
+  app = App.new()
+
+  puts "Welcome to School Library App!"
+  puts
+  puts
+  puts "Please choose an option by entering a number:"
+  puts "1 - List all books"
+  puts "2 - List all people"
+  puts "3 - Create a person"
+  puts "4 - Create a book"
+  puts "5 - Create a rental"
+  puts "6 - List all rentals for a given person id"
+  puts "7 - Exit"
+  
+  app.run()
+end
+
+main()
