929d14f9a81f4de30b7f748fe78f18ef222230a3
May Pyone <maypyone015@gmail.com>
2023-10-24 22:12:58 +0630

    Implement role and remove an argument from hadle_user method

--- student.rb
+++ student.rb
@@ -2,13 +2,19 @@ require './person'
 
 class Student < Person
   attr_accessor :classroom
+  attr_reader :role
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = nil
+    @role = 'Student'
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
   end
 end
+
+s = Student.new(12, 'John Doe', parent_permission: true)
+puts s.age
+puts s.role
--- teacher.rb
+++ teacher.rb
@@ -1,11 +1,12 @@
 require './person'
 
 class Teacher < Person
-  attr_reader :specialization
+  attr_reader :specialization, :role
 
   def initialize(age, specialization, name = 'Unknown')
     super(age, name)
     @specialization = specialization
+    @role = 'Teacher'
   end
 
   def can_use_services?
