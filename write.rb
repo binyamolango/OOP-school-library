82f33dd1cabcf5d195d9cc9dc04d7e75d88bcbaf
Muhammad Bilal Liaquat <bilal.liaquat7@gmail.com>
2023-10-25 15:13:01 +0500

    Update write.rb
    linters correction with manual spaces.

--- write.rb
+++ write.rb
@@ -3,27 +3,27 @@ require 'json'
 def write_file(data, file)
 	case File.basename(file)
 	when 'person.json'
-		if File.exist?(file)
-			File.open(file, 'w') { |f| f.puts JSON.generate(data) }
-		else
-			new_file_name = "person.json"
-			File.open(new_file_name, 'w') { |f| f.puts JSON.generate(data) }
-		end
+	  if File.exist?(file)
+	    File.open(file, 'w') { |f| f.puts JSON.generate(data) }
+	  else
+	    new_file_name = "person.json"
+	    File.open(new_file_name, 'w') { |f| f.puts JSON.generate(data) }
+	  end
 	when 'book.json'
 		if File.exist?(file)
-			File.open(file, 'w') { |f| f.puts JSON.generate(data) }
+		  File.open(file, 'w') { |f| f.puts JSON.generate(data) }
 		else
-			new_file_name = "book.json"
-			File.open(new_file_name, 'w') { |f| f.puts JSON.generate(data) }
+		  new_file_name = "book.json"
+		  File.open(new_file_name, 'w') { |f| f.puts JSON.generate(data) }
 		end
 	when 'rental.json'
 		if File.exist?(file)
-			File.open(file, 'w') { |f| f.puts JSON.generate(data) }
+		  File.open(file, 'w') { |f| f.puts JSON.generate(data) }
 		else
-			new_file_name = "rental.json"
-			File.open(new_file_name, 'w') { |f| f.puts JSON.generate(data) }
+		  new_file_name = "rental.json"
+		  File.open(new_file_name, 'w') { |f| f.puts JSON.generate(data) }
 		end
 	else
-		puts "Unrecognized file name: #{File.basename(file)}"
+	  puts "Unrecognized file name: #{File.basename(file)}"
 	end
 end
