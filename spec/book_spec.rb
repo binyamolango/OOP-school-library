671dcde1471a610fcabd46de47ba67a489a20dbf
binyamolango <olangobinyam@gmail.com>
2023-10-27 07:56:29 +0300

    Add test for add_rental method in the book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,11 +1,22 @@
 require './book'
+require './rental'
+require './person'
 
 describe Book do
+  book = Book.new('Saung', 'Hnin')
+
   context 'Creating a book' do
-    it 'Is a book created?' do
-      book = Book.new('Saung', 'Hnin')
+
+    it 'should create a book properly' do
       expect(book.title).to eq('Saung')
       expect(book.author).to eq('Hnin')
+    end
+
+    it 'should add books to the rentals array when add_rental method is called' do
+      person = Person.new(28, "Michael")
+      rental = Rental.new("20/10/2020", book, person)
+      book.add_rental(rental)
+      expect(book.rentals).to include rental
     end
   end
 end
