094555c1dda9fd8e7dd144169661b04dfcf60164
binyamolango <olangobinyam@gmail.com>
2023-10-27 08:01:19 +0300

    Seprate the unit tests for capitalize and trimmer decorators

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,15 @@
+require './nameable'
+require './person'
+
+describe CapitalizeDecorator do
+  person1 = Person.new(26, 'bereket')
+  capitalized_name = CapitalizeDecorator.new(person1)
+
+  context 'A capitalized_name object' do
+
+    it 'should return a capitilized name when we call correct_name method' do
+      expect(capitalized_name.correct_name).to eq 'Bereket'
+    end
+
+  end
+end
